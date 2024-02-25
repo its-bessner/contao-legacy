b8a444b21384f3905cc32ca865bd718d0aa322d4
Harrie <hbessner@googlemail.com>
2024-02-25 12:14:10 +0100

    Add initialize.php file to itsbessner/contao-bundle
    This new initialize.php file includes the Installer class with postCreate method responsible for rendering the composer.json in camel case. It also removes dummy files post creation, splits input vendors and bundles into different components for clean organization.

---
+++ initialize.php
@@ -0,0 +1,41 @@
+<?php
+class Installer {
+
+    public static function postCreate() {
+        unlink(__DIR__ . "/Dummy.php");
+
+        preg_match("/.*\/(?'vendor'.*)\/(?'bundle'.*)\/src$/", __DIR__, $matches);
+
+        $vendor = self::toCamelCase($matches["vendor"]);
+        $bundle = self::toCamelCase($matches["bundle"]);
+
+
+        $json = json_decode(file_get_contents(__DIR__ . "/../composer.json"));
+        unset($json->scripts);
+        $json->type = "contao-bundle";
+        $json->name = $matches["vendor"] . "/" . $matches["bundle"];
+        $json->description = "";
+        $json->autoload = (object) ["psr-4" => (object) [$vendor . "\\" . $bundle . "\\" => "src/"]];
+
+        file_put_contents(__DIR__ . "/../composer.json", json_encode($json, JSON_UNESCAPED_SLASHES));
+
+
+
+
+        var_dump($vendor, $bundle);
+
+        print str_repeat(PHP_EOL, 2);
+
+    }
+
+    public static function toCamelCase($input) {
+        $parts = preg_split("/[-_]/", $input);
+        foreach($parts as &$part) {
+            $part = ucfirst($part);
+        }
+        return implode('', $parts);
+    }
+
+}
+
+Installer::postCreate();
