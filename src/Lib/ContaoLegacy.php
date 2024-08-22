49394d7fb429423df6ff0ba3ee394e0c2a7dbf81
Harrie <hbessner@googlemail.com>
2024-08-22 05:30:17 +0200

    Refactor `alive` method and add `isBE` to ContaoLegacy
    Refactor the `alive` method to return a boolean instead of terminating the script. Additionally, introduce the `isBE` method to determine if the current request is a backend request. This enhances the functionality and readability of the code.

--- src/Lib/ContaoLegacy.php
+++ src/Lib/ContaoLegacy.php
@@ -1,11 +1,32 @@
 <?php
-
 namespace ItsBessner\ContaoLegacy\Lib;
-
+use Contao\System;
+use Symfony\Component\HttpFoundation\Request;
 class ContaoLegacy {
 
+    /**
+     * Indicates if the software is alive.
+     *
+     * This method returns a boolean value indicating if the software is alive or not.
+     *
+     * @return bool Returns true if the software is alive, false otherwise.
+     */
     static function alive() {
-        die("I am alive");
+       return true;
+    }
+
+
+    /**
+     * Returns whether the current request is a backend request or not.
+     *
+     * @return boolean True if the current request is a backend request, false otherwise.
+     */
+    public static function isBE()
+    {
+        return (System::getContainer()->get('contao.routing.scope_matcher')
+            ->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))
+        );
+
     }
 
 
